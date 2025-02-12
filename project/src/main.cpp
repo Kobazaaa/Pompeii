79f24ec6d63d4793353306d3b1fe91d2ea01e2fa
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-02-12 12:23:41 +0100

    Window setup with GLFW

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -4,17 +4,34 @@
 #include <stdexcept>
 #include <cstdlib>
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+constexpr uint32_t g_WIDTH = 800;
+constexpr uint32_t g_HEIGHT = 600;
+
 class HelloTriangleApplication
 {
 public:
 	void Run()
 	{
+		InitWindow();
 		InitVulkan();
 		MainLoop();
 		Cleanup();
 	}
 
 private:
+	void InitWindow()
+	{
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		m_pWindow = glfwCreateWindow(g_WIDTH, g_HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
 	void InitVulkan()
 	{
 		
@@ -22,13 +39,20 @@ private:
 
 	void MainLoop()
 	{
-		
+		while (!glfwWindowShouldClose(m_pWindow))
+		{
+			glfwPollEvents();
+		}
 	}
 
 	void Cleanup()
 	{
-		
+		glfwDestroyWindow(m_pWindow);
+
+		glfwTerminate();
 	}
+
+	GLFWwindow* m_pWindow{ nullptr };
 };
 
 int main()
