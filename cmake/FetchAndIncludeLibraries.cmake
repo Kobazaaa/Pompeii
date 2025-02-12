ef1d2dc7e550f65bbc7ceba94001d485a1371085
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-02-12 11:28:10 +0100

    Project and CMake setup

---
+++ cmake/FetchAndIncludeLibraries.cmake
@@ -0,0 +1,32 @@
+find_package(Vulkan REQUIRED)
+
+
+include(FetchContent)
+
+# Fetch GLFW
+FetchContent_Declare(
+   GLFW
+   GIT_REPOSITORY https://github.com/glfw/glfw.git
+   GIT_TAG 3.4
+   GIT_SHALLOW TRUE
+   GIT_PROGRESS TRUE
+)
+
+# Fetch GLM
+FetchContent_Declare(
+   GLM
+   GIT_REPOSITORY https://github.com/g-truc/glm.git
+   GIT_TAG 1.0.1
+   GIT_SHALLOW TRUE
+   GIT_PROGRESS TRUE
+)
+
+FetchContent_MakeAvailable(GLFW GLM)
+
+# Link libraries to the project
+target_link_libraries(${PROJECT_NAME}
+    PRIVATE
+    Vulkan::Vulkan
+    glfw
+    glm::glm
+)
