d3e146f6ff770386628d49d1412229c450c78fdd
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-03-02 21:21:50 +0100

    Implement function to read compiled shader files

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -12,6 +12,7 @@
 #include <optional>
 #include <set>
 #include <algorithm>
+#include <fstream>
 
 constexpr uint32_t g_WIDTH = 800;
 constexpr uint32_t g_HEIGHT = 600;
@@ -578,6 +579,21 @@ private:
 		}
 
 		return true;
+	}
+
+	static std::vector<char> ReadFile(const std::string& filename)
+	{
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+		if (!file.is_open())
+			throw std::runtime_error("Failed to open file!");
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+
+		file.close();
+		return buffer;
 	}
 
 	static VKAPI_ATTR VkBool32 VKAPI_CALL DebugCallback(
