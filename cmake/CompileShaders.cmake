add69427587d967987013bbfe350c1ebe1a0c1bf
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-03-02 17:40:55 +0100

    Compile shaders on build using CMake

---
+++ cmake/CompileShaders.cmake
@@ -0,0 +1,48 @@
+#--------------------------------------------------
+#    SHADER COMPILATION
+#--------------------------------------------------
+
+# Check if Vulkan SDK has already been found, if not, shaders cannot be compiled
+if(NOT Vulkan_FOUND)
+    message(FATAL_ERROR "Vulkan not found yet, cannot compile shaders!")
+endif()
+
+# Find the glslc executable
+find_program(GLSLC_EXECUTABLE glslc HINTS "${Vulkan_GLSLC_EXECUTABLE}")
+if(NOT GLSLC_EXECUTABLE)
+    message(FATAL_ERROR "glslc not found! Make sure Vulkan SDK is installed and available in PATH.")
+else()
+    message("glslc.exe found at ${GLSLC_EXECUTABLE}")
+endif()
+
+# Set shader directories
+set(SHADER_SOURCE_DIR ${CMAKE_SOURCE_DIR}/shaders)
+set(SHADER_BINARY_DIR ${CMAKE_BINARY_DIR}/shaders)
+file(MAKE_DIRECTORY ${SHADER_BINARY_DIR})
+
+# Shader Files
+file(GLOB SHADER_SOURCES
+  ${SHADER_SOURCE_DIR}/*.vert
+  ${SHADER_SOURCE_DIR}/*.frag
+)
+
+# Compile the shaders
+add_custom_target(CompileShaders ALL COMMENT "Compiling shaders to output directory.")
+foreach(SHADER ${SHADER_SOURCES})
+    get_filename_component(SHADER_INPUT_NAME ${SHADER} NAME)
+    string(REPLACE "." "_" SHADER_OUTPUT_NAME ${SHADER_INPUT_NAME})
+    set(SHADER_OUTPUT_DIR "${CMAKE_BINARY_DIR}/shaders/${SHADER_OUTPUT_NAME}.spv")
+
+    message("Compiling ${SHADER_INPUT_NAME} to ${SHADER_OUTPUT_NAME}.spv")
+
+    add_custom_command(
+        TARGET CompileShaders POST_BUILD
+        COMMAND ${GLSLC_EXECUTABLE} ${SHADER} -o ${SHADER_OUTPUT_DIR}
+        DEPENDS ${SHADER}
+        COMMENT "Compiling ${SHADER_INPUT_NAME} to ${SHADER_OUTPUT_NAME}.spv"
+        VERBATIM
+    )
+endforeach()
+
+# Add CompileShaders as a dependency to project
+add_dependencies(${PROJECT_NAME} CompileShaders)
