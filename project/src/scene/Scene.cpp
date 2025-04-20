66eb65ad69cd14f1878bbbee06ee7c2520c97ecd
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-04-20 16:44:52 +0200

    Add Directional Light + Scene class

---
+++ project/src/scene/Scene.cpp
@@ -0,0 +1,25 @@
+
+// -- Pompeii Includes --
+#include "Scene.h"
+
+//--------------------------------------------------
+//    Constructor & Destructor
+//--------------------------------------------------
+void pom::Scene::Load(const std::string& path)
+{
+	model.LoadModel(path);
+	directionalLight = { /* direction */	{ 0.25f, -1, 0 },
+						 /* color */		{ 1.f, 1.f, 1.f },
+						 /* intensity */	1.f,
+						 /* size */		{ 5000.f, 5000.f },
+						 /* near-far */	{ 0.1f, 11000.f },
+						 /* distance */	10000.f };
+}
+void pom::Scene::Allocate(const Context& context, CommandPool& cmdPool, bool keepHostData)
+{
+	model.AllocateResources(context, cmdPool, keepHostData);
+}
+void pom::Scene::Destroy()
+{
+	model.Destroy();
+}
---
+++ project/src/scene/Scene.h
@@ -0,0 +1,25 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+// -- Pom Includes --
+#include "DirectionalLight.h"
+#include "Model.h"
+
+namespace pom
+{
+	struct Scene
+	{
+		//--------------------------------------------------
+		//    Constructor & Destructor
+		//--------------------------------------------------
+		Scene() = default;
+		void Load(const std::string& path);
+		void Allocate(const Context& context, CommandPool& cmdPool, bool keepHostData = false);
+		void Destroy();
+
+		Model model;
+		DirectionalLight directionalLight;
+	};
+}
+
+#endif // SCENE_H
