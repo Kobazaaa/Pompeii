9a345c3908105cdeab7b6a2db5c527f583fe7eeb
Kobe Dereyne <kobe.dereyne@outlook.com>
2025-05-17 10:50:12 +0200

    Add function to retrieve worldpos from depth

--- project/shaders/helpers.glsl
+++ project/shaders/helpers.glsl
@@ -23,3 +23,19 @@ float GeometrySchlickGGX(in vec3 n, in vec3 v, in float k)
 	float nDotv = max(dot(n, v), 0.0);
 	return nDotv / (nDotv * (1-k) + k);
 }
+
+vec3 GetWorldPositionFromDepth(in float depth, in vec2 fragCoords, in vec2 resolution, in mat4 invProj, in mat4 invView)
+{
+	vec2 ndc = vec2(
+					(fragCoords.x / resolution.x) * 2.0 - 1.0,
+					(fragCoords.y / resolution.y) * 2.0 - 1.0
+				   );
+	ndc.y *= -1.0;
+	const vec4 clipPos = vec4(ndc, depth, 1.0);
+
+	vec4 viewPos = invProj * clipPos;
+	viewPos /= viewPos.w;
+
+	vec4 worldPos = invView * viewPos;
+	return worldPos.xyz;
+}
